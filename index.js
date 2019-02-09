1bf7ecc340eec862926eb4c5a767c30c35f9e0b6
Huy Phan <huy.phan@gmail.com>
2019-02-09 10:31:41 -0800

    initial committed

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log ('Hello from Node.js on GIT!....');
+// const person = require('./person');
+const Person = require('./person');
+
+const person1 = new Person('Huy',40);
+
+person1.greeting();
+
+console.log(Person);
---
+++ person.js
@@ -0,0 +1,20 @@
+// const person = {
+//     name: 'Huy Phan',
+//     age: 30
+// }
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greeting() {
+        console.log(`My name is  ${this.name} and I am ${this.age}`);
+
+    }
+}
+
+
+// module.exports = person;
+module.exports = Person;
